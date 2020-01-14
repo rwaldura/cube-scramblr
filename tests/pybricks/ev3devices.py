3fbcd382f7ee4a0f80a052e37c1d82cd1f1a8207
Ren Waldura <rwaldura@rwaldura-macbookpro3.roam.corp.google.com>
2020-01-14 15:16:42 -0800

    mock objects

--- tests/pybricks/ev3devices.py
+++ tests/pybricks/ev3devices.py
@@ -1,26 +1,27 @@
+# mock objects for unit testing
 
 class Motor :
     # Motor(Port.B, Direction.CLOCKWISE, [12, 36])
     def __init__(self, port, direction=0, gears=None) :
-        return
+        self._angle = 90
     def angle(self) : 
-        return 90
+        return self._angle
     def reset_angle(self, value) :
-        return
+        self._angle = value
     def run(self, speed, direction=0, stop=0) :
-        return 
+        self._angle += 10 
     def run_angle(self, speed, angle, stop=0, wait=True) :
-        return
+        self._angle += angle
     def run_target(self, speed, angle, stop=0, wait=True) :
-        return
+        self._angle = angle
     def run_until_stalled(self, speed) :
-        return
+        self._angle = 99
     def stop(self) :
-        return 
+        pass 
 
 class ColorSensor :
     def __init__(self, port) :
-        return
+        pass
     def color(self) :
         return None
     def rgb(self) :
