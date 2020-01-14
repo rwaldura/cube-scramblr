4bd3ee91b0ff08242ed5eef28a16960d8b33e1fe
Ren Waldura <rwaldura@rwaldura-macbookpro3.roam.corp.google.com>
2020-01-14 14:38:03 -0800

    tests pass with debugging output

--- tests/pybricks/tools.py
+++ tests/pybricks/tools.py
@@ -1,7 +1,15 @@
 import logging
 
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.DEBUG)
+
 def print(*args) :
-    logging.info(args)
+    strings = map(
+        lambda item: str(item),
+        args
+    )
+    logging.info(" ".join(strings))
 
 def wait(ms) :
     return
