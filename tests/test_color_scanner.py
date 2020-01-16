16fbb6ed03af1677c8aeda031abe9df33979ac06
Ren Waldura <rwaldura@rwaldura-macbookpro3.roam.corp.google.com>
2020-01-15 17:48:04 -0700

    negative test

--- tests/test_color_scanner.py
+++ tests/test_color_scanner.py
@@ -9,5 +9,11 @@ init()
 assertTrue("init", True)
 
 scan_cube_face_center(1)
-assertEqual("scan_cube_face_center", {'r':  0, 'g':  0, 'b':  0}, cube.center_rgb(0))
-assertEqual("scan_cube_face_center", {'r':100, 'g':100, 'b':100}, cube.center_rgb(1))
+
+# negative test
+assertEqual("scan_cube_face_center 0", {'r':  0, 'g':  0, 'b':  0}, cube.center_rgb(0))
+assertEqual("scan_cube_face_center 0", Color.BLACK, cube.center(0))
+
+assertEqual("scan_cube_face_center 1", {'r':100, 'g':100, 'b':100}, cube.center_rgb(1))
+# scanner doesn't map colors
+assertEqual("scan_cube_face_center 1", Color.BLACK, cube.center(1))
